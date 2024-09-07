bbb61d0257a518691df58775826728da09b58294
Snorlax0815 <mrafeiner@student.tgm.ac.at>
2024-09-07 13:54:38 +0200

    removed dokka action + dependencies, added create jar action.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,6 @@ plugins {
     id("org.jetbrains.dokka") version "1.9.20"
 }
 
-subprojects {
-    apply(plugin = "org.jetbrains.dokka")
-}
-
 group = "tgm.mrafeiner"
 version = "1.0-SNAPSHOT"
 
@@ -20,17 +16,11 @@ dependencies {
     implementation("org.json:json:20240303")
     // https://mvnrepository.com/artifact/org.mockito/mockito-core
     testImplementation("org.mockito:mockito-core:5.13.0")
-    // https://mvnrepository.com/artifact/org.jetbrains.dokka/dokka-gradle-plugin
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.20")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
-tasks.dokkaHtml{
-    outputDirectory.set(layout.buildDirectory.dir("dokka"))
-}
-
 
 kotlin {
     jvmToolchain(21)
