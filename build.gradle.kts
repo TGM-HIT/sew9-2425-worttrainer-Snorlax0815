111501d1777ec4c8ee66d2fbf5cd01e45d33e4b4
Snorlax0815 <mrafeiner@student.tgm.ac.at>
2024-09-07 10:31:02 +0200

    changed github action to dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
 plugins {
     kotlin("jvm") version "2.0.10"
+    id("org.jetbrains.dokka") version "1.9.20"
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.dokka")
 }
 
 group = "tgm.mrafeiner"
@@ -15,12 +20,18 @@ dependencies {
     implementation("org.json:json:20240303")
     // https://mvnrepository.com/artifact/org.mockito/mockito-core
     testImplementation("org.mockito:mockito-core:5.13.0")
-
+    // https://mvnrepository.com/artifact/org.jetbrains.dokka/dokka-gradle-plugin
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.20")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+tasks.dokkaHtml{
+    outputDirectory.set(layout.buildDirectory.dir("dokka"))
+}
+
+
 kotlin {
     jvmToolchain(21)
 }
